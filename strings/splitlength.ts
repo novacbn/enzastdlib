886be42c3daacc60b47cee3e7a1931744364d163
novacbn <31122674+novacbn@users.noreply.github.com>
2023-07-14 08:26:18 -0400

    - [@enzastdlib/strings] Added missing docstrings and examples.

--- strings/splitlength.ts
+++ strings/splitlength.ts
@@ -11,6 +11,27 @@ const EXPRESSION_WORDS = /\s*[^\s]+/g;
  * @param text
  * @param max_length
  * @returns
+ *
+ * @example
+ * ```typescript
+ * import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+ * import { splitLength } from 'https://deno.land/x/enzastdlib/strings/mod.ts';
+ *
+ * // **NOTE**: Taken from `deno lint --help`.
+ * const text =
+ *     'The configuration file can be used to configure different aspects of deno including TypeScript, linting, and code formatting. Typically the configuration file will be called `deno.json` or `deno.jsonc` and automatically detected; in that case this flag is not necessary.\nSee https://deno.land/manual@v1.33.0/getting_started/configuration_file';
+ *
+ * assertEquals(
+ *     splitLength(text, 66),
+ *     [
+ *         'The configuration file can be used to configure different aspects of',
+ *         'deno including TypeScript, linting, and code formatting. Typically',
+ *         'the configuration file will be called `deno.json` or `deno.jsonc` and',
+ *         'automatically detected; in that case this flag is not necessary.',
+ *         'See https://deno.land/manual@v1.33.0/getting_started/configuration_file',
+ *     ];
+ * );
+ * ```
  */
 export function splitLength(text: string, max_length: number): string[] {
 	const lines = text.split(EXPRESSION_NEWLINE);
