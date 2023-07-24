1170cb2374b73fa58ee5a5e44d146c1c6b7fc8a7
novacbn <31122674+novacbn@users.noreply.github.com>
2023-07-24 11:28:30 -0400

    - [@enzastdlib/json5] Added missing docstrings and examples. - [@enzastdlib/json5] Added TODO for creating tests.

--- json5/TODO.md
+++ json5/TODO.md
@@ -1,5 +1,4 @@
 # TODO
 
-- Add docstrings.
-
+- Add tests for `parseJSON5ExpressionRecord`.
 - Vendor dependencies into single file importable scripts.
--- json5/record.ts
+++ json5/record.ts
@@ -2,8 +2,72 @@ import { parseJSON5Expression } from './expression.ts';
 
 import type { JSONSchema, JSONSchemaObject } from '../schema/mod.ts';
 
+/**
+ * Represents a record of JSON5 expressions.
+ *
+ * @example
+ * ```typescript
+ * import type { JSON5ExpressionRecord } from 'https://deno.land/x/enzastdlib/json5/mod.ts';
+ *
+ * const record = {
+ *     myArray: `'Hello', 'World!'`,
+ * } satisfies JSON5ExpressionRecord;
+ * ```
+ */
 export type JSON5ExpressionRecord = Record<string, string | undefined>;
 
+/**
+ * Returns an expression parsed following a simplified JSON5 syntax using a
+ * a JSON Schema for type hinting.
+ *
+ * > **WARNING**: Only top-level keys are used for type hinting during parsing.
+ *
+ * @param schema
+ * @param record
+ * @returns
+ *
+ * @exanoke
+ * **schema.ts**
+ * ```typescript
+ * import type { JSONSchema, typeofschema } from 'https://deno.land/x/enzastdlib/schema/mod.ts';
+ *
+ * export const MY_SCHEMA = {
+ *     type: 'object',
+ *
+ *     required: ['_'],
+ *     additionalProperties: false,
+ *
+ *     properties: {
+ *         myArray: {
+ *             type: 'array',
+ *
+ *             items: {
+ *                 type: 'string',
+ *             },
+ *         },
+ *     },
+ * } as const satisfies JSONSchema;
+ *
+ * export type MySchemaType = typeofschema<typeof MY_SCHEMA>;
+ * ```
+ *
+ * **mod.ts**
+ * ```typescript
+ * import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+ * import { parseJSON5ExpressionRecord } from 'https://deno.land/x/enzastdlib/json5/mod.ts';
+ *
+ * import type { MySchemaType } from "./schema.ts";
+ * import { MY_SCHEMA } from "./schema.ts";
+ *
+ * const parsed = parseJSON5ExpressionRecord<MySchemaType>(MY_SCHEMA, {
+ *     myArray: `'Hello', 'World!'`,
+ * });
+ *
+ * assertEquals(parsed, {
+ *     myArray: ['Hello', 'World!'],
+ * });
+ * ```
+ */
 export function parseJSON5ExpressionRecord<Type>(
 	schema: JSONSchemaObject,
 	record: JSON5ExpressionRecord,
