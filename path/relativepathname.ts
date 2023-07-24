d49810537e9ddaab0f94bbf8a8831d0d55bf78ad
novacbn <31122674+novacbn@users.noreply.github.com>
2023-07-24 10:45:11 -0400

    - [@enzastdlib/path] Added missing docstrings and examples.

--- path/relativepathname.ts
+++ path/relativepathname.ts
@@ -8,6 +8,20 @@ const { relative } = posix;
  * @param from
  * @param to
  * @returns
+ *
+ * @example
+ * ```typescript
+ * import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+ * import { relativePathname } from 'https://deno.land/x/enzastdlib/path/mod.ts';
+ *
+ * const URL_A = new URL("https://example.domain/assets");
+ * const URL_B = new URL("https://example.domain/assets/scripts/main.js");
+ *
+ * assertEquals(
+ *     relativePathname(URL_A, URL_B),
+ *     'scripts/main.js'
+ * );
+ * ```
  */
 export function relativePathname(from: URL, to: URL): string {
 	return relative(from.pathname, to.pathname);
